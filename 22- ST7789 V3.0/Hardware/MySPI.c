b180cfb5c3342c52d899639f0d0c20da9428185d
12698 <lyshgd@gmail.com>
2025-04-13 03:19:14 +0800

    SDVedio

--- 22- ST7789 V3.0/Hardware/MySPI.c
+++ 22- ST7789 V3.0/Hardware/MySPI.c
@@ -9,7 +9,7 @@ void MySPI_Init(void){
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA|RCC_APB2Periph_SPI1, ENABLE);
 	
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_11 | GPIO_Pin_12 ;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_11 | GPIO_Pin_12 |GPIO_Pin_3;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 	GPIO_Init(GPIOA, &GPIO_InitStructure);
 	
---
+++ 25- ST7789_SPI2 V4.0/Hardware/ST7789.h
@@ -0,0 +1,17 @@
+#ifndef _ST7789_H
+#define _ST7789_H
+
+void ST7789_Write_Bus(uint8_t Data);
+void ST7789_Write_Data8(uint8_t Data);
+void ST7789_Write_Data16(uint16_t Data);
+void ST7789_Write_Cmd8(uint8_t Cmd);
+void ST7789_Write_Cmd_Data16(uint8_t Cmd,uint16_t Data);
+void ST7789_Address_Set(uint16_t x1,uint16_t y1,uint16_t x2,uint16_t y2);
+void ST7789_Init(void);
+
+
+void LCD_Clear(uint16_t Color);
+void LCD_DrawPoint(uint16_t x,uint16_t y,uint16_t Color);
+void LCD_Fill(uint16_t x_Start,uint16_t y_Strat,uint16_t x_End,uint16_t y_End,uint16_t Color);
+
+#endif
